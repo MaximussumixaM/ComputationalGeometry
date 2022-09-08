686e942656d2f3b24438876beeebd02d2aa9f92c
MaximussumixaM <anurovmaxim@gmail.com>
2022-09-08 19:35:17 +0300

    Curve zero-rule

--- ComputationalGeometry/CGCurve.cpp
+++ ComputationalGeometry/CGCurve.cpp
@@ -2,45 +2,7 @@
 #include "CGCurve.h"
 
 namespace CGBase_NS {
-							// === Constructors === //
-	CGCurve2d::CGCurve2d() {
-
-		Curve = std::make_shared<CGVertex2d_V>();
-		EntType = EntityType::POLYLINE;	
-	}
-
-	CGCurve2d::CGCurve2d(int n) {
-
-		Curve = std::make_shared<CGVertex2d_V>();
-		Curve->resize(n);
-			
-		EntType = EntityType::POLYLINE;		
-	}	
-	
-	CGCurve2d::CGCurve2d(const CGCurve2d& cur) {
-	
-		Curve = cur.Curve;
-		//Curve->clear();
-		//for (auto i = Curve->begin(); i != Curve->end(); i++) {
-			//this->Add(*i);
-		//}
-		EntType = EntityType::POLYLINE;		
-	}	
-
-	CGCurve2d::CGCurve2d(const CGVertex2d_V& v_cur) {
-		
-		
-		Curve = std::make_shared<CGVertex2d_V>();
-		
-		EntType = EntityType::POLYLINE;
-	}
-					//===Add Vertex to the end of curve===//	
-	void
-		CGCurve2d::Add(CGVertex2d& v) {
-		
-			Curve->push_back(v);	
-	}
-	
+							
 						//===Get vertex by index===//
 	CGVertex2d*
 		CGCurve2d::GetVertex(int i) {
--- ComputationalGeometry/CGCurve.h
+++ ComputationalGeometry/CGCurve.h
@@ -8,51 +8,38 @@
 namespace CGBase_NS {
 								// === CGCurve2d === //
 	class CGCurve2d : CGBase
-	{   
+	{  
+//Using Zero - idiom(all constructors default)
 	public:
-		//CGVertex2d_V* Curve;
-		std::shared_ptr<CGVertex2d_V> Curve;
-	
 
-		CGCurve2d();//Default constructor
-		CGCurve2d(int);                            
-		CGCurve2d(const CGCurve2d&);//Copy constructor
-		CGCurve2d(const CGVertex2d_V&);
-		CGCurve2d& operator=  (const CGCurve2d& cur) {//Assignment operator	
-
-			for (auto i = cur.Curve->begin(); i != cur.Curve->end(); i++) {
-				this->Add(*i);
-			}
+		CGCurve2d(): //Default constructor
+		Curve(std::make_shared<CGVertex2d_V>())
+		{			
 			EntType = EntityType::POLYLINE;
-
-			return *this;
-		}
-		CGCurve2d(CGCurve2d&& cur)noexcept : Curve(std::move(cur.Curve))  {
-
-			//Curve->clear();
-			Curve = std::move(cur.Curve);
-			EntType = EntityType::POLYLINE;
-			//Может сделать Curve.Clear, но в базовом clear уже есть
-			cur.Curve = nullptr;
-			cur.EntType = EntityType::BASE;
-
-		}
-
-		CGCurve2d& operator=(CGCurve2d&& cur) noexcept
-		{
-			//assert(this != &cur); // assert if this is not a temporary
-			if (this != &cur)
-			{
-				Curve = std::move(cur.Curve);
-			}
-			cur.Curve = nullptr;
-			cur.EntType = EntityType::BASE;
-
-			return *this;
-		}
-		~CGCurve2d() {
-			//delete Curve;
 		};
+
+		CGCurve2d(int n):
+		Curve(std::make_shared<CGVertex2d_V>())
+		{			
+			Curve->resize(n);
+			EntType = EntityType::POLYLINE;
+		};
+		CGCurve2d(const CGCurve2d&) = default;//Copy constructor
+
+		CGCurve2d& operator=(const CGCurve2d&) = default;
+
+		CGCurve2d(CGCurve2d&&) = default;
+
+		CGCurve2d& operator=(CGCurve2d&&) = default;
+		
+		CGCurve2d(const CGVertex2d_V& v) 
+		:CGCurve2d()
+
+		{
+			Curve = std::make_shared<CGVertex2d_V>(CGVertex2d_V(v));
+		};		
+		
+		~CGCurve2d() = default;
 
 		size_t Size() { return Curve->size(); }//Number of vertecies
 		bool isEmpty();//Line is empty
@@ -61,7 +48,10 @@ namespace CGBase_NS {
 		void Add(CGVertex2d& v);//Add Vertex to the end of curve	
 
 		CGVertex2d* GetVertex(int);//Get vertex by index
-		CGVertex2d* GetLastVertex();//Get last vertex		
+		CGVertex2d* GetLastVertex();//Get last vertex	
+
+	public:
+		std::shared_ptr<CGVertex2d_V> Curve;
 	};
 }
 #endif
